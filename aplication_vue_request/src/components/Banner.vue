87f9778c36a3b4abd305f5c300dd17bb5cbe09cd
PedroPenhaa <pedro.eng98@gmail.com>
2024-02-01 15:22:31 -0300

    banner home

--- aplication_vue_request/src/components/Banner.vue
+++ aplication_vue_request/src/components/Banner.vue
@@ -13,5 +13,21 @@
 <style scoped>
     .main-banner{
         background-image: url('/public/img/burger.jpg');
+        background-position: 0 -250px;
+        background-size: cover;
+        height: 640px;
+        display: flex;
+        align-items: center;
+
     }
+
+    .main-banner h1{
+        color: #FFF;
+        text-align: center;
+        font-size: 60px;
+        background-color: #222;
+        padding: 20px 60px;
+    }
+
+    
 </style>
